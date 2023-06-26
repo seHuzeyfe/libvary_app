7ab618336d16fcab4b300ad8a3c7fe67e5b5e91e
Huzeyfe Bicakci <huzeyfebicakci@gmail.com>
2023-06-26 03:44:15 +0300

    Welcome page tamamlandı

---
+++ lib/routs.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/widgets.dart';
+import 'package:libvary_app/View/welcome_screen.dart';
+
+final Map<String , WidgetBuilder> routes ={
+  WelcomeScreen.routeName: (context) => WelcomeScreen(),
+};
